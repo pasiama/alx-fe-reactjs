018e656138cffef3bd70b214ffe251eea20aeb29
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-08-15 20:46:07 +0000

    Assignment 2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,6 +6,7 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import Header from './components/Header'
+import Counter from './components/Counter'
 
 function App() {
  
@@ -17,6 +18,7 @@ function App() {
     <MainContent />
  
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <Counter />
     <Footer />
     </>
   )
