f2f8c98eca3d737d11293977e3539aeff2391168
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-08-17 22:34:05 +0000

    move nav bar to components

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -8,7 +8,7 @@ import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 import Footer from './Footer';
 
 function App() {
