e24ab6a512982ef139f1ae662547b6d1e4a1298b
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-08-18 20:00:52 +0000

    Add context

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
+import  { useContext } from 'react';
+import UserContext from './components/UserContext';
 
-const UserProfile = (props) => {
+const UserProfile = () => {
+      const userData = useContext(UserContext);
                   return (
                         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-                              <h2 style={{ color: 'blue' }}>{props.name}</h2>
-                              <p>Age:<span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-                              <p>Bio: {props.bio}</p>
+                              <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+                              <p>Age:<span style={{ fontWeight: 'bold' }}>{userData.age}</span></p>
+                              <p>Bio: {userData.bio}</p>
                         </div>
                   );
             };
