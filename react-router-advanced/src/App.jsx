99e1abad8b5bda121f0a920d2a4524cdf996cfc8
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-09-01 12:25:54 +0000

    fixing bugs

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }
 />
 
-<Route path="/post/:postId" element={<BlogPost />} />
+<Route path="/blog/:id" element={<BlogPost />} />
 
       </Routes>
     </Router>
--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -2,12 +2,12 @@
 import { useParams } from 'react-router-dom';
 
 const BlogPost = () => {
-  const { postId } = useParams();
+      const { id } = useParams();
 
   return (
     <div>
-      <h1>Blog Post {postId}</h1>
-      <p>This is the content for blog post with ID: {postId}</p>
+      <h1>Blog Post {id }</h1>
+      <p>This is the content for blog post with ID: {id }</p>
     </div>
   );
 };
