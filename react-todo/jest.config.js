d2c1bf5571bfd72f113609b8f8900c23a2cb6968
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-09-08 16:24:34 +0000

    updates

---
+++ react-todo/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+      transform: {
+        "^.+\\.(js|jsx)$": "babel-jest"
+      },
+      // Add this if you're using ESM modules
+      extensionsToTreatAsEsm: [".jsx"],
+      // Other Jest configurations
+    };
+    
