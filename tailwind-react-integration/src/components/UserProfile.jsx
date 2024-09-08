49d96c69e58e942ac9128249a17c5905ccd63121
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-09-08 23:31:16 +0000

    update 5

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,16 +1,19 @@
 function UserProfile() {
       return (
-        <div className="user-profile sm:p-4 md:text-xl bg-gray-100 p-4 md:max-w-sm md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-14 md:my-20 rounded-lg shadow-lg">
-          <img 
-            src="https://via.placeholder.com/150" 
-            alt="User" 
-            className="w-24 sm:w-24 sm:h-24 h-24 md:w-36 md:h-36 rounded-full mx-auto" 
-          />
-          <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
-          <p className="text-sm sm:text-base md:text-lg text-gray-600">
-            Developer at Example Co. Loves to write code and explore new technologies.
-          </p>
-        </div>
+            <div className="bg-gray-100 p-4 sm:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <img
+              src="https://via.placeholder.com/150"
+              alt="User"
+              className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+            />
+            <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300">
+              John Doe
+            </h1>
+            <p className="text-sm md:text-base text-gray-600">
+              Developer at Example Co. Loves to write code and explore new technologies.
+            </p>
+          </div>
+          
       );
     }
     
