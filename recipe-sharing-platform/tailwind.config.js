9ab7d79554e5bb362c2340cef0f91acb5899f0c1
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-09-15 13:20:58 +0000

    Receipe test

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
