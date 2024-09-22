2cfb5d35cca7581c327fbbf413114abd590cbe85
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-09-22 22:43:10 +0000

    add tailwind

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+export const content = [
+  './src/**/*.{js,jsx,ts,tsx}', // This will scan all your components for Tailwind classes
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
