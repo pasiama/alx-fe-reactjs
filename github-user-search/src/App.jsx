1eb6fc1fcd3473b2beead5d7bfbe78fe29ba6bf1
Asiama Phoebe <asiama.phoebe@hubtel.com>
2024-09-22 21:05:23 +0000

    Exercise one

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      {/* Components like SearchInput and UserList will go here */}
+    </div>
+  );
+}
+
+export default App;
